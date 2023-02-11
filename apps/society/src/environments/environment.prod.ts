105811a120ea16b613a765e698d8ad8f1a3d1e5a
nabeelsahmed <nabeel.ahmed@aimspakistan.edu.pk>
2023-02-11 15:15:26 +0500

    changes in environment

--- apps/society/src/environments/environment.prod.ts
+++ apps/society/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  apiUrl: 'http://157.90.101.251:8050/',
+  // apiUrl: 'http://157.90.101.251:8050/',
+  apiUrl: 'http://192.168.10.100:8050/',
   imageUrl: 'C:/inetpub/wwwroot/SrcSocietyProject/society/assets/ui/',
-  imageSavedPath: 'http://157.90.101.251:8060/assets/ui/',
+  // imageSavedPath: 'http://157.90.101.251:8060/assets/ui/',
+  imageSavedPath: 'http://192.168.10.100:8060/assets/ui/',
 };
