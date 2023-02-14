d966316e35690560eb22e033750ff3903032e438
AdnanAli7 <adnanali1990@gmail.com>
2023-02-14 21:45:27 +0500

    changes by adnan

--- apps/society/src/environments/environment.ts
+++ apps/society/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   envName: 'prod',
-  apiUrl: 'http://157.90.101.251:8050/',
-  // apiUrl: 'http://192.168.10.100:8050/',
+  //apiUrl: 'http://157.90.101.251:8050/',
+   apiUrl: 'http://192.168.10.100:8050/',
   // apiUrl: 'http://localhost:5000/',
   imageUrl: 'C:/inetpub/wwwroot/SrcSocietyProject/society/assets/ui/',
   // imageSavedPath: 'http://5.196.171.59:7000/assets/ui/',
-  imageSavedPath: 'http://157.90.101.251:8060/assets/ui/',
-  // imageSavedPath: 'http://192.168.10.100:8060/assets/ui/',
+  //imageSavedPath: 'http://157.90.101.251:8060/assets/ui/',
+   imageSavedPath: 'http://192.168.10.100:8060/assets/ui/',
 };
 
 /*
